997cebb4ca66e5eb1710f5edc785acac92f74f07
CaptainE <s153758@student.dtu.dk>
2017-03-19 13:58:02 +0100

    because jesper

---
+++ LibraryTestData.sql
@@ -0,0 +1,7 @@
+Use library;
+
+/* Test data for libraries*/
+insert into `libraries` values (1,'Egedal Bibliotek','34H',3850,'Træhusevej',40000),
+(2,'Birkerød Bibliotek','12J',3850,'Træhusevej',12000),
+(3,'Thisted Bibliotek','42G',7000,'Pipedrejervej',20000);
+
