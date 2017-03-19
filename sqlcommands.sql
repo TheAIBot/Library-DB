f83d9441a1bf1c4b6a7bc95cf6f56a1ce6c858e2
TheAIBot <andreasuranus@hotmail.com>
2017-03-19 13:23:18 +0100

    changed functions to procedures

--- sqlcommands.sql
+++ sqlcommands.sql
@@ -1,30 +1,23 @@
 #--Procedues--
 	#--loaner--
+		#create new loaner
 		#get the articles that a loaners is currently borrowing with remaining time visible
 		#get a loaners loaning history
         #get all loans that a loaner returned too soon
 	#--librarian--
-		#get all articles that a librarian is currently resposible for
-		#get all articles a librarian has loaned out
-    #--library--
-		#get a librarys books
-		#get librarians that work for a specific library
-        #get all loaners that currently loan an article from that library
-	#--article--
-
-
-#--Functions--
-	#--loaner--
-		#create new loaner
-	#--librarian--
 		#create new librarian
         #loan article to loaner
         #return article that a loaner loaned
+		#get all articles that a librarian is currently resposible for
+		#get all articles a librarian has loaned out
     #--library--
 		#move article from one library to another
         #move librarian from one libraryto another
-    #--article--
-		#create new article
-        
+		#get a librarys books
+		#get librarians that work for a specific library
+        #get all loaners that currently loan an article from that library
+        #create new article
+	#--article--
+    
         
 #--triggers--
